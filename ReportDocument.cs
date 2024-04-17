0ac2534cdec80aee742bfc819c74629077e9ad8f
Patrick Tong <tongchakpupatrick@gmail.com>
2024-04-17 14:12:18 -0400

    Create document for the Report PDF

---
+++ ReportDocument.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace FinancialManagementSystem
+{
+    internal class ReportDocument : IDocument
+    {
+        public ReportDocumentDataSource source { get; }
+
+        public ReportDocument(ReportDocumentDataSource source )
+        {
+            this.source = source;
+        }
+
+        public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+        public DocumentSettings GetSettings() => DocumentSettings.Default;
+
+        public void Compose(IDocumentContainer container)
+        {
+            container
+                .Page(page =>
+                {
+                    page.Margin(50);
+
+                    page.Header().Element(ComposeHeader);
+                    page.Content().Element(ComposeContent);
+                    page.Header().Height(100).Background(Colors.Grey.Lighten1);
+                    page.Content().Background(Colors.Grey.Lighten3);
+                    page.Footer().Height(50).Background(Colors.Grey.Lighten1);
+
+
+                    page.Footer().AlignCenter().Text(x =>
+                    {
+                        x.CurrentPageNumber();
+                        x.Span(" / ");
+                        x.TotalPages();
+                    });
+                });
+        }
+
+        void ComposeHeader(IContainer container)
+        {
+            var titleStyle = TextStyle.Default.FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
+
+            container.Row(row =>
+            {
+                row.RelativeItem().Column(column =>
+                {
+                    column.Item().Text($"Financial Report").Style(titleStyle);
+
+                    column.Item().Text(text =>
+                    {
+                        text.Span("Issue date: ").SemiBold();
+                        text.Span($"{DateTime.Now.ToString("dd-MM-yyyy")}");
+                    });
+                });
+
+                row.ConstantItem(100).Height(50).Placeholder();
+            });
+        }
+
+        void ComposeContent(IContainer container)
+        {
+            container
+                .PaddingVertical(40)
+                .Height(250)
+                .Background(Colors.Grey.Lighten3)
+                .AlignCenter()
+                .AlignMiddle()
+                .Text("Content").FontSize(16);
+        }
+    }
+}
---
+++ ReportDocumentDataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuestPDF.Helpers;
+
+namespace FinancialManagementSystem
+{
+    public class ReportDocumentDataSource
+    {
+        private DataTable table;
+        private User User;
+        public ReportDocumentDataSource(DataTable table)
+        {
+            User = CurrentUser.User;
+            this.table = table;
+
+        }
+    }
+}
